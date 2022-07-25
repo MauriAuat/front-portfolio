f481f020ac9ada174aa87821b40dc6c4ec0a662f
MauricioAuat <mauri.auat@gmail.com>
2022-07-25 17:59:54 -0300

    autenticacion funcionando login component listo

--- src/app/componentes/auth/login2.component.ts
+++ src/app/componentes/auth/login2.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginUsuario } from 'src/app/modelos/login-usuario';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-login2',
   templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.css']
+  styleUrls: ['./login2.component.css'],
 })
 export class Login2Component implements OnInit {
+  isLogged = false;
+  isLoginFail = false;
+  loginUsuario: LoginUsuario = new LoginUsuario('', '');
+  nombreUsuario: string = '';
+  password: string = '';
+  roles: string[] = [];
+  errMsj: string = '';
 
-  constructor() { }
+  constructor(
+    private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    if (this.tokenService.getToken()) this.isLogged = true;
+    this.isLoginFail = false;
+    this.roles = this.tokenService.getAuthorities();
   }
 
+  onLogin(): void {
+    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+    this.authService.login(this.loginUsuario).subscribe(
+      (data) => {
+        this.isLogged = true;
+        this.isLoginFail = false;
+
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        this.router.navigate(['/']);
+      },
+      (err) => {
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errMsj = err.error.mensaje;
+        console.log(this.errMsj + 'hola');
+      }
+    );
+  }
 }
